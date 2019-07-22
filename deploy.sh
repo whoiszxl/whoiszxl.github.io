95efe867838dd1d0646be0e44e33e9bfe880c519
zhouxiaolong <whoiszxl@gmail.com>
2019-07-22 18:32:20 +0800

    update blog.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-bundle exec jekyll build JEKYLL_ENV=production --destination ../whoiszxl.github.io/ && cd ../whoiszxl.github.io/ && git add . && git commit -m "update blog." && git push origin master
+jekyll build --destination ../whoiszxl.github.io/ && cd ../whoiszxl.github.io/ && git add . && git commit -m "update blog." && git push origin master
